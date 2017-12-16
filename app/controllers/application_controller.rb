74a83ce9953758ffe132be54594ab5f785ed068c
sevdeaybuke <sevdeaybuke@outlook.com>
2017-12-16 21:59:47 +0000

    first

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,17 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :set_locale
+    def set_locale
+      I18n.locale=params[:locale] ||
+      I18n.default_locale
+    end
+  before_action :configure_permitted_parameters,if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+  end
+    
+end
